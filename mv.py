8056e9c704f0e3754e64486bfac46b717a535c63
sebasmos <ulsordonez@unicauca.edu.co>
2022-11-21 21:51:25 -0500

    testing default

--- mv.py
+++ mv.py
@@ -1,5 +1,4 @@
 from rockstar import RockStar
 
-python_code = "print('CardioLux')"
-rock_it_bro = RockStar(days=400, file_name='CardioLux.py', code=python_code)
+rock_it_bro = RockStar(days=500)
 rock_it_bro.make_me_a_rockstar()
